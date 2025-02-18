ac388d96e1d230c23891384363461807e9b9ea1c
Tim Machnacki <machnackit@gmail.com>
2025-02-17 20:03:11 -0500

    stuff

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -2,7 +2,13 @@ import { cn } from "@/lib/utils";
 import { Loader2Icon, type LucideProps } from "lucide-react";
 
 export function Spinner({ className, ...props }: LucideProps) {
-  return <Loader2Icon className={cn("animate-spin", className)} {...props} />;
+  return (
+    <Loader2Icon
+      className={cn("animate-spin", className)}
+      aria-label={props["aria-label"] || "Loading"}
+      {...props}
+    />
+  );
 }
 
 // TODO: maybe use custom spinner? Will have to adjust spacing on a lot of components to match svg
@@ -12,7 +18,7 @@ export function Spinner({ className, ...props }: LucideProps) {
 //     <div
 //       data-slot="spinner"
 //       className={cn(
-//         "relative mr-0.5 inline-flex size-4 items-center justify-center",
+//         "relative m-0.5 inline-flex size-4 items-center justify-center",
 //         className,
 //       )}
 //       aria-label={props["aria-label"] || "Loading"}
