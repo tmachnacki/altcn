418405dbdfc96e1a4844c0f4b92028ec686b9f8d
Tim Machnacki <machnackit@gmail.com>
2025-02-27 16:30:50 -0500

    alerts might be done

---
+++ components/icons/success-filled.tsx
@@ -0,0 +1,24 @@
+// credit: https://heroicons.com
+
+import { cn } from "@/lib/utils";
+
+export function SuccessFilledIcon({
+  className,
+  ...props
+}: React.ComponentProps<"svg">) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="none"
+      className={cn("size-5 fill-current", className)}
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
