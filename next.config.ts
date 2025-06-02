55864881f85da607189422a08735a72cbe62fa3d
Tim Machnacki <machnackit@gmail.com>
2025-06-01 21:06:14 -0400

    init fumadocs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import { createMDX } from "fumadocs-mdx/next";
+
+const withMDX = createMDX();
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
